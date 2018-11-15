6b48bbbb341963035432f665ab6bcba35d2a850c
Treger Alexander <a.treger@corp.badoo.com>
2018-11-15 23:44:44 +0300

    iframe experiment

---
+++ user_script.js
@@ -0,0 +1,6 @@
+/**
+ * user_script.js
+ */
+
+console.log('This is user javascript');
+document.body.style.backgroundColor = 'red';
---
+++ user_script2.js
@@ -0,0 +1,6 @@
+/**
+ * user_script.js
+ */
+
+console.log('This is user javascript number TWO');
+document.body.style.backgroundColor = 'blue';
